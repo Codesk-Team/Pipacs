0beac4d0d227d2d07ced69db6c7d3d3466534b0a
Péter Röhberg <rohbergpeter@gmail.com>
2023-02-06 15:35:30 +0100

    Done with new Menu page

---
+++ src/components/SheepMenu.js
@@ -0,0 +1,165 @@
+import { useTranslation } from "react-i18next";
+
+const SheepMenu = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div class="vc_tta-panels-container">
+      <div class="vc_tta-panels">
+        <div
+          class="vc_tta-panel vc_active"
+          id="1501871514474-98fac6cb-b327"
+          data-vc-content=".vc_tta-panel-body"
+        >
+          <div class="vc_tta-panel-body">
+            <div class="mgt-header-block clearfix text-center text-black wpb_animate_when_almost_visible wpb_fadeInUp fadeInUp wpb_content_element mgt-header-block-style-2 mgt-header-block-fontsize-regular mgt-header-texttransform-none mgt-header-block-42722978">
+              <h2 class="mgt-header-block-title text-font-weight-default">
+                {t("menuPage.sheepMenu.title")}
+              </h2>
+              <div class="mgt-header-line mgt-header-line-margin-small"></div>
+            </div>
+            <div class="wpb_text_column wpb_content_element wpb_animate_when_almost_visible wpb_fadeInUp fadeInUp vc_custom_1501952538196 text-size-medium">
+              <div class="wpb_wrapper" style={{ textAlign: "center" }}>
+                <p style={{ textAlign: "center" }}>
+                  <span style={{ color: "#999999" }}>
+                    <em>{t("menuPage.sheepMenu.subTitle1")}</em>
+                  </span>
+                </p>
+                <p style={{ textAlign: "center" }}>
+                  <span style={{ color: "#999999" }}>
+                    <em>{t("menuPage.sheepMenu.subTitle2")}</em>
+                  </span>
+                </p>
+                <p style={{ textAlign: "center" }}>
+                  <span style={{ color: "#999999" }}>
+                    <em>{t("menuPage.sheepMenu.subTitle3")}</em>
+                  </span>
+                </p>
+              </div>
+            </div>
+            <div class="vc_row wpb_row vc_inner vc_row-fluid">
+              <div class="wpb_column vc_column_container vc_col-sm-12">
+                <div class="vc_column-inner">
+                  <div class="wpb_wrapper" style={{ textAlign: "center" }}>
+                    <div class="mgt-header-block clearfix text-left text-black wpb_animate_when_almost_visible wpb_fadeInLeft fadeInLeft wpb_content_element mgt-header-block-style-2 mgt-header-block-fontsize-regular mgt-header-texttransform-header mgt-header-block-60691344">
+                      <h2
+                        class="mgt-header-block-title text-font-weight-default"
+                        style={{ textAlign: "center" }}
+                      >
+                        {t("menuPage.sheepMenu.menuName")}
+                      </h2>
+                    </div>
+                    <div class="vc_separator wpb_content_element vc_separator_align_center vc_sep_width_100 vc_sep_border_width_3 vc_sep_pos_align_center vc_separator_no_text vc_sep_color_black wpb_animate_when_almost_visible wpb_fadeInLeft fadeInLeft wpb_animate_when_almost_visible wpb_fadeInLeft fadeInLeft">
+                      <span class="vc_sep_holder vc_sep_holder_l">
+                        <span class="vc_sep_line"></span>
+                      </span>
+                      <span class="vc_sep_holder vc_sep_holder_r">
+                        <span class="vc_sep_line"></span>
+                      </span>
+                    </div>
+                    <div class="mgt-item-price mgt-item-price-badge-color-red clearfix wpb_content_element wpb_animate_when_almost_visible wpb_fadeInUp fadeInUp">
+                      <div class="mgt-item-price-details">
+                        <h4>
+                          <b>{t("menuPage.sheepMenu.meal1")} </b>
+                          <span
+                            style={{
+                              fontSize: 18,
+                              fontFamily: "cabin",
+                            }}
+                          >
+                            - 3460 Ft
+                          </span>
+                        </h4>
+                      </div>
+                    </div>
+                    <div class="mgt-item-price mgt-item-price-badge-color-orange clearfix wpb_content_element wpb_animate_when_almost_visible wpb_fadeInUp fadeInUp">
+                      <div class="mgt-item-price-details">
+                        <h4>
+                          <b>{t("menuPage.sheepMenu.meal2")} </b>
+                          <span
+                            style={{
+                              fontSize: 18,
+                              fontFamily: "cabin",
+                            }}
+                          >
+                            - 2860 Ft (2020 Ft)
+                          </span>
+                        </h4>
+                      </div>
+                    </div>
+                    <div class="mgt-item-price mgt-item-price-badge-color-red clearfix wpb_content_element wpb_animate_when_almost_visible wpb_fadeInUp fadeInUp">
+                      <div class="mgt-item-price-details">
+                        <h4>
+                          <b>{t("menuPage.sheepMenu.meal3")} </b>
+                          <span
+                            style={{
+                              fontSize: 18,
+                              fontFamily: "cabin",
+                            }}
+                          >
+                            - 4580 Ft
+                          </span>
+                        </h4>
+                      </div>
+                    </div>
+                    <div class="mgt-item-price mgt-item-price-badge-color-red clearfix wpb_content_element wpb_animate_when_almost_visible wpb_fadeInUp fadeInUp">
+                      <div class="mgt-item-price-details">
+                        <h4>
+                          <b>{t("menuPage.sheepMenu.meal4")} </b>
+                          <span
+                            style={{
+                              fontSize: 18,
+                              fontFamily: "cabin",
+                            }}
+                          >
+                            - 7460 Ft
+                          </span>
+                        </h4>
+                      </div>
+                    </div>
+                    <div class="mgt-item-price mgt-item-price-badge-color-red clearfix wpb_content_element wpb_animate_when_almost_visible wpb_fadeInUp fadeInUp">
+                      <div class="mgt-item-price-details">
+                        <h4>
+                          <b>{t("menuPage.sheepMenu.meal5")} </b>
+                          <span
+                            style={{
+                              fontSize: 18,
+                              fontFamily: "cabin",
+                            }}
+                          >
+                            - 6260 Ft (4380 Ft)
+                          </span>
+                        </h4>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="wpb_text_column wpb_content_element wpb_animate_when_almost_visible wpb_fadeInUp fadeInUp vc_custom_1501952538196 text-size-medium">
+              <div class="wpb_wrapper" style={{ textAlign: "center" }}>
+                <p style={{ textAlign: "center" }}>
+                  <span style={{ color: "#999999" }}>
+                    <em>{t("menuPage.sheepMenu.additionalText1")}</em>
+                  </span>
+                </p>
+                <p style={{ textAlign: "center" }}>
+                  <span style={{ color: "#999999" }}>
+                    <em>{t("menuPage.sheepMenu.additionalText2")}</em>
+                  </span>
+                </p>
+                <p style={{ textAlign: "center" }}>
+                  <span style={{ color: "#999999" }}>
+                    <em>{t("menuPage.sheepMenu.additionalText3")}</em>
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SheepMenu;
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -2,63 +2,10 @@ import React from "react";
 import Footer from "../layout/Footer";
 import Header from "../layout/Header";
 import { useTranslation } from "react-i18next";
+import SheepMenu from "../components/SheepMenu";
 
 const Menu = () => {
   const { t, i18n } = useTranslation();
-
-  const menuSlicer = (text, huMaxLength, enMaxLength) => {
-    const textLenght = text.length;
-    let maxLength = i18n.language === "hu" ? huMaxLength : enMaxLength;
-
-    if (!maxLength) {
-      maxLength = 25;
-    }
-
-    return (
-      <>
-        {textLenght > maxLength ? (
-          <>
-            {text.slice(0, maxLength)}
-            <br />
-            {text.slice(maxLength, textLenght)}
-          </>
-        ) : (
-          <>{text}</>
-        )}
-      </>
-    );
-  };
-
-  const threeLinerMenuSlicer = (
-    text,
-    huMaxLengthOne,
-    huMaxLengthTwo,
-    enMaxLengthOne,
-    enMaxLengthTwo
-  ) => {
-    const textLenght = text.length;
-    let maxLengthOne = i18n.language === "hu" ? huMaxLengthOne : enMaxLengthOne;
-
-    if (!maxLengthOne) {
-      maxLengthOne = 15;
-    }
-
-    let maxLengthTwo = i18n.language === "hu" ? huMaxLengthTwo : enMaxLengthTwo;
-
-    if (!maxLengthTwo) {
-      maxLengthTwo = 15;
-    }
-
-    return (
-      <>
-        {text.slice(0, maxLengthOne)}
-        <br />
-        {text.slice(maxLengthOne, maxLengthTwo)}
-        {/* <br /> */}
-        {text.slice(maxLengthTwo, textLenght)}
-      </>
-    );
-  };
 
   return (
     <div class="home page-template-default page page-id-3699 wpb-js-composer js-comp-ver-5.2.1 vc_responsive">
@@ -130,6 +77,15 @@ const Menu = () => {
                   <div class="wpb_column vc_column_container vc_col-sm-12">
                     <div class="vc_column-inner">
                       <div class="wpb_wrapper">
+                        <SheepMenu />
+                        <div class="vc_separator wpb_content_element vc_separator_align_center vc_sep_width_100 vc_sep_pos_align_center vc_separator_no_text vc_sep_color_grey">
+                          <span class="vc_sep_holder vc_sep_holder_l">
+                            <span class="vc_sep_line"></span>
+                          </span>
+                          <span class="vc_sep_holder vc_sep_holder_r">
+                            <span class="vc_sep_line"></span>
+                          </span>
+                        </div>
                         <div class="vc_tta-container" data-vc-action="collapse">
                           <div class="vc_general vc_tta vc_tta-tabs vc_tta-color-grey vc_tta-style-outline vc_tta-shape-rounded vc_tta-o-shape-group vc_tta-o-no-fill vc_tta-tabs-position-top vc_tta-controls-align-center">
                             <div class="vc_tta-tabs-container">
@@ -189,44 +145,7 @@ const Menu = () => {
                                       </h2>
                                       <div class="mgt-header-line mgt-header-line-margin-small"></div>
                                     </div>
-                                    <div class="wpb_text_column wpb_content_element wpb_animate_when_almost_visible wpb_fadeInUp fadeInUp vc_custom_1501952538196 text-size-medium">
-                                      <div class="wpb_wrapper">
-                                        <p style={{ textAlign: "center" }}>
-                                          <span style={{ color: "#999999" }}>
-                                            <em>
-                                              {/* Felhívjuk kedves vendégeink
-                                              figyelmét, hogyha van zarojel
-                                              akkor az a feladag (MEGFOGALMAZNI
-                                              VHOGY) */}
-                                              {t("menuPage.warningHalfPrice")}
-                                            </em>
-                                          </span>
-                                        </p>
-                                        <p style={{ textAlign: "center" }}>
-                                          <span style={{ color: "#999999" }}>
-                                            <em>
-                                              {/* Feltüntetett árainkra +10%
-                                              szervízdíjat számolunk fel, melyet
-                                              számlája végösszege tartalmaz. */}
-                                              {t("menuPage.servicePrice")}
-                                            </em>
-                                          </span>
-                                        </p>
-                                        <p style={{ textAlign: "center" }}>
-                                          <span style={{ color: "#999999" }}>
-                                            <em>
-                                              {/* Az étlapon jelölve találják az
-                                              egyes ételek allergén tartalmát! A
-                                              további allergén információkkal
-                                              kapcsolatos tájékoztatásban kérje
-                                              Kollegáink segítségét! Köszönjük! */}
-                                              {t("menuPage.allergies")}
-                                            </em>
-                                          </span>
-                                        </p>
-                                      </div>
-                                    </div>
-                                    <div class="vc_row wpb_row vc_inner vc_row-fluid vc_custom_1501952123390">
+                                    <div class="vc_row wpb_row vc_inner vc_row-fluid">
                                       <div class="wpb_column vc_column_container vc_col-sm-6">
                                         <div class="vc_column-inner">
                                           <div class="wpb_wrapper">
@@ -992,6 +911,29 @@ const Menu = () => {
                                             </div>
                                           </div>
                                         </div>
+                                      </div>
+                                    </div>
+                                    <div class="wpb_text_column wpb_content_element wpb_animate_when_almost_visible wpb_fadeInUp fadeInUp vc_custom_1501952538196 text-size-medium">
+                                      <div class="wpb_wrapper">
+                                        <p style={{ textAlign: "center" }}>
+                                          <span style={{ color: "#999999" }}>
+                                            <em>
+                                              {t("menuPage.warningHalfPrice")}
+                                            </em>
+                                          </span>
+                                        </p>
+                                        <p style={{ textAlign: "center" }}>
+                                          <span style={{ color: "#999999" }}>
+                                            <em>
+                                              {t("menuPage.servicePrice")}
+                                            </em>
+                                          </span>
+                                        </p>
+                                        <p style={{ textAlign: "center" }}>
+                                          <span style={{ color: "#999999" }}>
+                                            <em>{t("menuPage.allergies")}</em>
+                                          </span>
+                                        </p>
                                       </div>
                                     </div>
                                   </div>
